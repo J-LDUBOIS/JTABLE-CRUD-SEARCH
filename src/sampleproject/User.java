63f144cf3cf350a07414a37d72cb46406bf98c94
dubs <dubs@MacBook-Pro-de-Mathilde.local>
2018-02-19 14:53:33 +0100

    1er commit

---
+++ src/sampleproject/User.java
@@ -0,0 +1,42 @@
+package sampleproject;
+
+/**
+ *
+ * @author dubs
+ */
+public class User {
+    
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    
+    public User(int ID, String FirstName, String LastName, int Age)
+    {
+        this.id = ID;
+        this.firstName = FirstName;
+        this.lastName = LastName;
+        this.age = Age;
+    }
+    
+    public int getId()
+    {
+        return id;
+    }
+    
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    
+    public String getLastName()
+    {
+        return lastName;
+    }
+    
+    public int getAge()
+    {
+        return age;
+    }
+}
+
